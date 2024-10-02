1d1c54a0217e25529f029075ab438a94b04e2bc5
Julien <zigan.julien@gmail.com>
2024-10-02 16:07:15 +0200

    implement argument handling with std::env::args()

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 fn main() {
-    let message = std::env::args();
+    let message = std::env::args()
+        .nth(1)
+        .expect("Missing the message. Usage: catsay <message>");
 
-    for argument in message {
-        println!("{}", argument);
-    }
+    println!();
+    println!("{}", message);
+    println!("  \\");
+    println!("   \\");
+    println!("     /\\_/\\");
+    println!("    ( o o )");
+    println!("    =( I )=");
+    println!();
 }
