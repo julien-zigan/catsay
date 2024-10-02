1e7fd8fe2dfa49f4edc2e379f82fa51b325c56ff
Julien <zigan.julien@gmail.com>
2024-10-02 17:56:32 +0200

    adding flag -d --dead with [clap(short, long)] to make dead eyes.

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,24 @@ struct Options {
     #[clap(default_value = "Meow!")]
     /// What the cat says
     message: String,
+
+    #[clap(short = 'd', long = "dead")]
+    /// Make the cat appear dead
+    dead: bool,
 }
 
 fn main() {
     let options = Options::parse();
     let message = options.message;
 
+    let eye= if options.dead {"x"} else {"o"};
+
     println!();
     println!("{}", message);
     println!("  \\");
     println!("   \\");
     println!("     /\\_/\\");
-    println!("    ( o o )");
+    println!("    ( {eye} {eye} )");
     println!("    =( I )=");
     println!();
 }
