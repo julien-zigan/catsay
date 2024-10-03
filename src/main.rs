8fbae932444c7882ced53bbd5690cd08dfff3089
Julien <zigan.julien@gmail.com>
2024-10-03 16:07:01 +0200

    add from file function, add catfile.txt

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,10 @@ struct Options {
     #[clap(short = 'd', long = "dead")]
     /// Make the cat appear dead
     dead: bool,
+
+    #[clap(short = 'f', long = "file")]
+    /// Load the cat picture from the specified file
+    catfile: Option<std::path::PathBuf>,
 }
 
 fn main() {
@@ -20,14 +24,26 @@ fn main() {
         eprint!("A cat shouldn't bark!")
     }
 
-    let eye= if options.dead {"x"} else {"o"};
+    let eye = if options.dead { "x".red().bold() } else { "o".bold()};
 
     println!();
-    println!("{}", message.yellow().bold().on_red());
-    println!("  \\");
-    println!("   \\");
-    println!("     /\\_/\\");
-    println!("    ( {0} {0} )", eye);
-    println!("    =( I )=");
+    println!("{}", message.red().bold().on_bright_yellow());
+
+    match &options.catfile {
+        Some(path) => {
+            let cat_template =
+                std::fs::read_to_string(path).expect(&format!("could not read file {:?}", path));
+            let eye = format!("{}", eye);
+            let cat_picture = cat_template.replace("{eye}", &eye);
+            println!("{}", &cat_picture);
+        }
+        None => {
+            println!("  \\");
+            println!("   \\");
+            println!("     /\\_/\\");
+            println!("    ( {0} {0} )", eye);
+            println!("    =( I )=");
+        }
+    }
     println!();
 }
